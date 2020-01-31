ab9d254cfaed9cbbcc6ef8eb78c0c3c278ba5eec
Ehiremen Ekore <ehisekore@outlook.com>
2020-01-30 20:11:09 -0500

    Initial Commit

---
+++ lab1_quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab1_quiz
+//
+//  Created by Ekore, Ehiremen Alex on 1/30/20.
+//  Copyright © 2020 Ekore, Ehiremen Alex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
